c05214138d55ed44b16a933dae02e0cabb7e1c84
Sam Bischoff <sam.bischoff2001@gmail.com>
2022-06-29 12:30:00 -0600

    added js

--- server/index.js
+++ server/index.js
@@ -6,6 +6,14 @@ const app = express()
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
+//css endpoint
+app.get('/css', (req, res) => {
+    res.sendFile(path.join(__dirname, '../styles.css'))
+})
+//js endpoint
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.js'))
+})
 
 const port = process.env.PORT || 4005
 
